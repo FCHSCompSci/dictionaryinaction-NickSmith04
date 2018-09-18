6f6e3f682296a70d5a36a073619329d019471723
Nicholas Smith <nicholas.smith4@stu.jefferson.kyschools.us>
2018-09-18 09:34:51 -0400

    "ill push"

--- videogamedictonary.py
+++ videogamedictonary.py
@@ -1,8 +1,37 @@
+question=input ("Hello! Welcome to Nicks Game Store! Todays special is street fighter 2! and its 50 Bucks!")
+
 Store_game = {
     'title':"Street Fighter 2.",
     'genre':"2D Fighting.",
     'release date':"1991.",
-    'cost':"50$.",
-    'platform':("Super Nintendo.")
+    'cost':50,
+    'platform':("Super Nintendo.")}
 
-    
+
+mystore = {'S': 'Street Fighter',}
+
+me = {
+    "money":100
+    }
+
+if Store_game["cost"] < me["money"]:
+    print("you got Street Fighter! You got 50 bucks left!")
+
+question=input ("Want Another game? Type Yes")
+
+Store_game = {
+    'title':"Street Fighter 2.",
+    'genre':"2D Fighting.",
+    'release date':"1991.",
+    'cost':50,
+    'platform':("Super Nintendo.")}
+
+
+mystore = {'S': 'Street Fighter',}
+
+me = {
+    "money":60
+    }
+
+if Store_game["cost"] < me["money"]:
+    print("Congrats!")
